cea76ff88fddf150d9d3675987ce0ee12bd785aa
Vishaal Panchhi <v_panchhi@hotmail.com>
2019-03-04 21:11:02 -0500

    fb fixes

--- src/data/zapier/api.js
+++ src/data/zapier/api.js
@@ -3,6 +3,9 @@ const request = require('request-promise');
 const config = require('../../config');
 
 class ZapierApiDataService {
+    constructor () {
+        this.ZAPIER_WEBHOOK_URL = config.zapier.webhookUrl;
+    }
     async save ({ name, token, email, adAccount, page }) {
         // TODO: zapier call
     }
