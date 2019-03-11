c16e07e90f71ced4dfbff9649a24c5fe3ecd6164
Ollie Cee <olliecee.mail@gmail.com>
2019-03-10 21:34:42 -0400

    FE updates

--- pages/_app.js
+++ pages/_app.js
@@ -23,12 +23,15 @@ class MyApp extends App {
                     <title>Kittys</title>
                     <meta name="viewport" content="width=device-width, initial-scale=1" />
                     <meta name="description" content="An agency that creates strategies that embrace what makes social marketing unique. With billions of impressions to his name, you won’t find many Facebook marketers with a greater depth of experience and insight."/>
-                    {/*<link href="https://fonts.googleapis.com/css?family=Lato:400,700" rel="stylesheet" />*/}
+                    <link rel="stylesheet"
+                          href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+                          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+                          crossOrigin="anonymous" />
+                    <link href="https://fonts.googleapis.com/css?family=Lato:400,700" rel="stylesheet" />
                 </Head>
                 <style jsx global>{`
                     body {
-                        // font-family: 'Lato', sans-serif;
-                        max-width: 450px;
+                        font-family: 'Lato', sans-serif;
                         width: 100%;
                         margin: 0 auto;
                     }
